a29477c75ea410d6f72aef1c07bdb08b86556bee
Ben Kettle <ben@semgrep.com>
2023-11-13 16:00:18 -0800

    sort git branches

--- shell/alias.sh
+++ shell/alias.sh
@@ -10,6 +10,7 @@ alias rm='rm -i'
 alias ec="emacsclient -nc"
 alias j="just"
 alias g="git"
+alias -g gitprune="git fetch -p ; git branch --merged | egrep -v '(main|develop|release|master)' | xargs git branch -d"
 
 # cd to git root directory
 alias cdgr='cd "$(git root)"'
@@ -22,7 +23,7 @@ mcd() {
 # git fuzzy checkout - from https://stackoverflow.com/questions/36513310/how-to-get-a-gits-branch-with-fuzzy-finder
 gfc() {
   local branches branch
-  branches=$(git branch -vv) &&
+  branches=$(git branch -vv --sort=-committerdate) &&
   branch=$(echo "$branches" | fzf +m) &&
   git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
 }
